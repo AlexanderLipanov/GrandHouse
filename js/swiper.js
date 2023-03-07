eed91ea2f51b5ec6b8aa7cb2d39b89b1b1d3f01c
Alexandr <lipanov162@gmail.com>
2023-03-08 01:03:38 +0300

    cottages

---
+++ js/swiper.js
@@ -0,0 +1,169 @@
+const swiperTimber = new Swiper('.swiper-timber', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    // width: 300,
+    loopPreventsSliding: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 3300,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        840: {
+            slidesPerView: 2,
+            spaceBetween: 32
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 48
+        }
+    }
+});
+
+const swiperFoam = new Swiper('.swiper-foam', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    // width: 300,
+    loopPreventsSliding: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 3300,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        840: {
+            slidesPerView: 2,
+            spaceBetween: 32
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 48
+        }
+    }
+});
+
+const swiperFrame = new Swiper('.swiper-frame', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    // width: 300,
+    loopPreventsSliding: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 3300,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        840: {
+            slidesPerView: 2,
+            spaceBetween: 32
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 48
+        }
+    }
+});
+
+const swiperCottage = new Swiper('.swiper-cottage', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    // width: 300,
+    loopPreventsSliding: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 3300,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        840: {
+            slidesPerView: 2,
+            spaceBetween: 32
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 48
+        }
+    }
+});
+
+const swiperBath = new Swiper('.swiper-bath', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    // width: 300,
+    loopPreventsSliding: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 3300,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        840: {
+            slidesPerView: 2,
+            spaceBetween: 32
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 48
+        }
+    }
+});
