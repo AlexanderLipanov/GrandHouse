4bd235c69af4cb9dca298307711b9933b45c2505
Alexandr <lipanov162@gmail.com>
2023-03-20 22:31:13 +0300

    bug fix

--- js/form.js
+++ js/form.js
@@ -1,50 +1,15 @@
 
 "use strict";
 
-let mail = {
-    name: "",
-    date: "",
-    comment: "",
-    number: "",
-    time: "",
-}
-
-
 let policyCheckbox = document.getElementById("policy-checkbox");
 let sendFormButton = document.getElementById("send");
 
 sendFormButton.disabled = true;
 function toggleCheckbox() {
     sendFormButton.disabled = !policyCheckbox.checked;
-}
-
-let inputName = document.getElementById("name-input");
-let inputDate = document.getElementById("date-input");
-let inputComment = document.getElementById("comment-input");
-let inputNumber = document.getElementById("number-input");
-let inputTime = document.getElementById("time-input");
-
-function inputNameChange() {
-    mail.name = inputName.value;
-}
-
-function inputDateChange() {
-    mail.date = inputDate.value;
-}
-
-function inputCommentChange() {
-    mail.comment = inputComment.value;
-}
-
-function inputNumberChange() {
-    mail.number = inputNumber.value;
-}
-
-function inputTimeChange() {    
-    mail.time = inputTime.value;
-}
-
-function sendMail() {
-    
-    console.log(mail);
+    if(!sendFormButton.disabled) {
+        sendFormButton.style.color = "black";
+    } else {
+        sendFormButton.style.color = "gray";
+    }
 }
